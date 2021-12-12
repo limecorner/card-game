a1e5ecfdbbea315c3ec7636ccb084a05878cb10b
Limecorner <iversonwang40@gmail.com>
2021-12-12 22:31:03 +0800

    change 1,11,12,13 to A J Q K

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
 const cardsElement = document.querySelector('#cards')
+const symbols = ['https://assets-lighthouse.alphacamp.co/uploads/image/file/17989/__.png'
+  , 'https://assets-lighthouse.alphacamp.co/uploads/image/file/17992/heart.png'
+  , 'https://assets-lighthouse.alphacamp.co/uploads/image/file/17991/diamonds.png'
+  , 'https://assets-lighthouse.alphacamp.co/uploads/image/file/17988/__.png']
+
 const view = {
-  displayCards(numbersOfCards) {
+  displayCards() {
+    const cardsArray = Array.from(Array(52).keys())
     let cardsHTML = ''
-    for (let numbersOfCard = 0; numbersOfCard < numbersOfCards; numbersOfCard++) {
-      cardsHTML += `<div class="card" style="width: 18rem;">
-      <p class="point">5</p>    
-      <img src="https://assets-lighthouse.alphacamp.co/uploads/image/file/17989/__.png" alt="">
-      <p class="point">5</p>
-    </div>`
-    }
+    cardsArray.forEach(index => {
+      cardsHTML += this.getCardContent(index)
+    })
     cardsElement.innerHTML = cardsHTML
+  },
+  getCardContent(index) {
+    const point = this.transformNumber((index % 13) + 1)
+    const symbol = symbols[Math.floor(index / 13)]
+    return `<div class="card">
+      <p class="point">${point}</p>    
+      <img src="${symbol}" alt="">
+      <p class="point">${point}</p>
+    </div>`
+  },
+  transformNumber(point) {
+    switch (point) {
+      case 1:
+        return 'A'
+      case 11:
+        return 'J'
+      case 12:
+        return 'Q'
+      case 13:
+        return 'K'
+    }
+    return point
   }
 }
 
-view.displayCards(52)
+view.displayCards()
